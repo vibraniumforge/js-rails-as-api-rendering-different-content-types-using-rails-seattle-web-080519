32aef4641481d8d58a372ffb43ce944b85d7d6b2
vibraniumforge <gregm9999@gmail.com>
2019-12-02 16:31:53 -0800

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,9 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render 'birds/index.html.erb'
+    # render 'birds/index.html.erb'
+    # render plain: "Hello #{@birds[3].name}"
+    # render json: 'Remember that JSON is just object notation converted to string data, so strings also work here'
+    render json: @birds
   end
 end
